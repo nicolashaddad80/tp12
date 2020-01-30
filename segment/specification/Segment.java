50b950f4e33c0cfeea55777144fe451b8540daac
Nicolas HADDAD <nicolas.haddad80@gmail.com>
2020-01-30 14:04:27 +0100

    Part#1 Code Complete for release

--- segment/specification/Segment.java
+++ segment/specification/Segment.java
@@ -1,7 +1,10 @@
 package fr.cnam.tp12.segment.specification;
 
 public interface Segment {
+
     double getLength();
 
     void translate(double dx, double dy);
+
+    void refreshLength();
 }
---
+++ tests/SegmentOptimizedTest.java
@@ -0,0 +1,50 @@
+package fr.cnam.tp12.tests;
+
+import fr.cnam.tp12.point.PointOptimized;
+import fr.cnam.tp12.point.specification.Point;
+import fr.cnam.tp12.segment.SegmentOptimized;
+import fr.cnam.tp12.segment.specification.Segment;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class SegmentOptimizedTest {
+
+    private final static double precision = 0.0001;
+
+    private Point p1;
+    private Point p2;
+    private Segment s1;
+
+
+    @Before
+    public void setUp() throws Exception {
+        this.p1 = new PointOptimized(0, 0);
+        this.p2 = new PointOptimized(0, 10);
+        this.s1 = new SegmentOptimized(p1, p2);
+    }
+
+    @Test
+    public void getLengthWithoutAnyTranslatingTest() {
+        //Must be pass in this Part #1 implementation
+        assertEquals(10, this.s1.getLength(), precision);
+    }
+
+    @Test
+    public void getLengthWithTranslatingSegment() {
+        //Must be pass in Part #1 implementation
+        //Do segment Translation
+        this.s1.translate(20, 20);
+        assertEquals(10, this.s1.getLength(), precision);
+    }
+
+    @Test
+    public void getLengthWithTranslatingPointOnly() {
+        //Must be fail in Part #1 implementation
+        //Do Point Translation
+        this.p1.translate(0, -10);
+        assertEquals(20, this.s1.getLength(), precision);
+    }
+}
