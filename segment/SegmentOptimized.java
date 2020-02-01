3b2b66ba43ad18558055c34910c31d7b5b8e77b6
Nicolas HADDAD <nicolas.haddad80@gmail.com>
2020-02-01 22:34:55 +0100

    Conformance with exercise#4.1 spec release condidate for V4.1.1

--- segment/SegmentOptimized.java
+++ segment/SegmentOptimized.java
@@ -1,15 +1,21 @@
 package fr.cnam.tp12.segment;
 
+import fr.cnam.tp12.mypatterns.MyObserver;
 import fr.cnam.tp12.point.PointObservable;
 import fr.cnam.tp12.segment.specification.Segment;
 
-public class SegmentOptimized implements Segment {
+import java.util.Observer;
+
+public class SegmentOptimized  implements Segment {
     /**
      * Attributes
      */
     private PointObservable p1;
 
     private PointObservable p2;
+
+    private MyObserver p1Observer;
+    private MyObserver p2Observer;
 
     /*
     Optimized
@@ -20,11 +26,13 @@ public class SegmentOptimized implements Segment {
      * Constructor
      */
     public SegmentOptimized(PointObservable a_P1, PointObservable a_P2) {
-
+        this.p1Observer=new SegmentLengthUpdate(this);
         this.p1 = a_P1;
-        this.p1.addObserver(this);
+        this.p1.addObserver(this.p1Observer);
+
+        this.p2Observer=new SegmentLengthUpdate(this);
         this.p2 = a_P2;
-        this.p2.addObserver(this);
+        this.p2.addObserver(p2Observer);
         this.updateLength();
     }
 
@@ -44,19 +52,14 @@ public class SegmentOptimized implements Segment {
         this.p2.translate(dx, dy);
     }
 
-    private void updateLength() {
+    public void updateLength() {
         this.length = this.p1.distance(this.p2);
     }
 
     @Override
-    public void update() {
-        this.updateLength();
-    }
-
-    @Override
     public void destroy() {
-            this.p1.deleteObserver(this);
-            this.p2.deleteObserver(this);
+            this.p1.deleteObserver(this.p1Observer);
+            this.p2.deleteObserver(this.p2Observer);
     }
 
 }
