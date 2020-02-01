b53786816a5573a2744a8a73a87bb63cb2431296
Nicolas HADDAD <nicolas.haddad80@gmail.com>
2020-02-01 23:04:20 +0100

    Exo#4-Part#2 internal Observer static class code and tests PASS complete

--- segment/SegmentOptimized.java
+++ segment/SegmentOptimized.java
@@ -4,9 +4,25 @@ import fr.cnam.tp12.mypatterns.MyObserver;
 import fr.cnam.tp12.point.PointObservable;
 import fr.cnam.tp12.segment.specification.Segment;
 
-import java.util.Observer;
+public class SegmentOptimized implements Segment {
 
-public class SegmentOptimized  implements Segment {
+    /**
+     * Internal Classes
+     */
+    private static class LengthUpdate implements MyObserver{
+        private SegmentOptimized segmentOptimized;
+
+        public LengthUpdate(SegmentOptimized segmentOptimized) {
+            this.segmentOptimized = segmentOptimized;
+        }
+
+        @Override
+        public void update() {
+            this.segmentOptimized.updateLength();
+        }
+    }
+
+
     /**
      * Attributes
      */
@@ -26,11 +42,11 @@ public class SegmentOptimized  implements Segment {
      * Constructor
      */
     public SegmentOptimized(PointObservable a_P1, PointObservable a_P2) {
-        this.p1Observer=new SegmentLengthUpdate(this);
+        this.p1Observer = new LengthUpdate(this);
         this.p1 = a_P1;
         this.p1.addObserver(this.p1Observer);
 
-        this.p2Observer=new SegmentLengthUpdate(this);
+        this.p2Observer = new LengthUpdate(this);
         this.p2 = a_P2;
         this.p2.addObserver(p2Observer);
         this.updateLength();
@@ -58,8 +74,8 @@ public class SegmentOptimized  implements Segment {
 
     @Override
     public void destroy() {
-            this.p1.deleteObserver(this.p1Observer);
-            this.p2.deleteObserver(this.p2Observer);
+        this.p1.deleteObserver(this.p1Observer);
+        this.p2.deleteObserver(this.p2Observer);
     }
 
 }
